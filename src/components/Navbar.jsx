effecbee866d56c2c4943336d59b7d68ff2dc583
Alimzy <salamialameen20@gmail.com>
2026-05-02 15:08:25 +0100

    created my  first components and also install my Tailwind Package

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav className="flex justify-between items-center px-10 py-4 border-b">
+      <h1 className="text-xl font-bold">SHOP.CO</h1>
+    </nav>
+  )
+}
+
+export default Navbar
