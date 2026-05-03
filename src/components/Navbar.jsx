c0f8eede497dad4085f291dd8cbc13abf248cf47
Alimzy <salamialameen20@gmail.com>
2026-05-03 05:53:17 +0100

    Added other Navbar list and work on my index.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,12 @@ function Navbar() {
   return (
     <nav className="flex justify-between items-center px-10 py-4 border-b">
       <h1 className="text-xl font-bold">SHOP.CO</h1>
+      <ul className="flex gap-6">
+        <li>Shop</li>
+        <li>On Sale</li>
+        <li>New Arrivals</li>
+        <li>Brands</li>
+      </ul>
     </nav>
   )
 }
