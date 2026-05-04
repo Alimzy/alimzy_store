177b1d0e9a6cb0330963bc28151477de65208b67
Alimzy <salamialameen20@gmail.com>
2026-05-04 11:48:30 +0100

    made some changes on Product Card and Create Top Selling file

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,7 @@ function ProductCard(props){
 
         <div className=" bg-gray-100 w-60 rounded-2xl overflow-hidden cursor-pointer hover:shadow-lg transition-shadow">    
             
-              <img src="/product1.png" className="w-full h-64 object-cover "/>
+              <img src="/product1.png" className="w-full h-64 object-cover bg-gray-100 "/>
               
               <div className="p-4">
                 <h3 className="font-medium text-sm">{props.name}</h3>
