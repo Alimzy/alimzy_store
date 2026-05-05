c94f6f9e886d89cec19cd541292ecbbbf00d2579
Alimzy <salamialameen20@gmail.com>
2026-05-05 13:12:34 +0100

    Started my footer and added some images to my public

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,36 @@
 function Finding() {
     return(
         <div>
-            <div></div>
+            <div>
+                <div>
+                    <H3>STAY UPTO DATE ABOUT OUR LATEST OFFERS</H3>
+                   <div>
+                     <input type="text" placeholder="Enter your email address" />
+                    <button>Subscribe to Newsletter</button> 
+                   </div>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <h3>SHOP.CO</h3>
+                    <P>We have clothes that suits your style and which you’re proud to wear. From women to men.</P>
+                    <div>
+                        <img src="./logo-twitter.png" alt="Twitter Logo" />
+                        <img src="./2.png" alt="Facebook Logo" />
+                        <img src="./3.png" alt="Instagram Logo" />
+                        <img src="./4.png" alt="" />
+                    </div>
+                </div>
+                <div>
+                    <h3>COMPANY</h3>
+                    <ul>
+                        <li>About</li>
+                        <li>Features</li>
+                        li          
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
